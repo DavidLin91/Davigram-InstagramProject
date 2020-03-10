b898203506efa1c518bf721520ae83d5dcc1a75c
David <lin.david985@gmail.com>
2020-03-10 09:21:03 -0400

    edited cv cell size

---
+++ Davigram-InstagramProject/Views/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Davigram-InstagramProject
+//
+//  Created by David Lin on 3/10/20.
+//  Copyright © 2020 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
